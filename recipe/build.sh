7327cff31fc3262544766fac4ec6f5272294ff96
Daniel Petry <dpetry@anaconda.com>
2023-10-26 17:11:36 -0500

    Bugfix: direct CMake towards mlir location

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,8 @@ set -ex
 # remove outdated vendored headers
 rm -rf $SRC_DIR/python/triton/third_party
 
+# direct CMake towards MLIR library directory
+export MLIR_DIR="$PREFIX/lib"
+
 cd python
 $PYTHON -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir -vv
