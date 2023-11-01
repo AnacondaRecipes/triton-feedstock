4952302321d3d546a0e806803fc894cca17af3b8
Daniel Petry <dpetry@anaconda.com>
2023-11-01 16:02:41 -0500

    Turn off optimizations

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,5 +8,8 @@ rm -rf $SRC_DIR/python/triton/third_party
 # direct CMake towards MLIR library directory
 export MLIR_DIR="$PREFIX/lib"
 
+export CFLAGS="$(echo $CFLAGS | sed 's/-O[0-9]//g')"
+export CXXFLAGS="$(echo $CXXFLAGS | sed 's/-O[0-9]//g')"
+
 cd python
 $PYTHON -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir -vv
