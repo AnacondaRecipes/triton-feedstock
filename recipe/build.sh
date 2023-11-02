17e40b8a05381a605f5f12002912c8a201fb88c7
Daniel Petry <dpetry@anaconda.com>
2023-11-02 15:31:09 -0500

    bugfix

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,10 +6,7 @@ set -ex
 rm -rf $SRC_DIR/python/triton/third_party
 
 # direct CMake towards LLVM library directory
-export MLIR_DIR="$PREFIX/lib"
-export LLVM_INCLUDE_DIRS="$SRC_DIR/include"
-export LLVM_LIBRARY_DIR="$SRC_DIR/lib"
-export LLVM_SYSPATH="$SRC_DIR"
+export LLVM_DIR="$SRC_DIR"
 
 export CFLAGS="$(echo $CFLAGS | sed 's/-O[0-9]//g')"
 export CXXFLAGS="$(echo $CXXFLAGS | sed 's/-O[0-9]//g')"
