f2ad92a3b8eecf15e4a8e129601425a5d789d754
Daniel Petry <dpetry@anaconda.com>
2023-11-07 17:24:06 -0600

    Try not using C++11 ABI

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,8 @@ rm -rf $SRC_DIR/python/triton/third_party
 
 # direct CMake towards LLVM library directory
 export LLVM_DIR="$SRC_DIR"
-
+# the vendored-in llvm on centos is compiled with the pre-C++11 ABI, leading to linker errors if we don't include this
+export GLIBCXX_USE_CXX11_ABI=0
 export CFLAGS="$(echo $CFLAGS | sed 's/-O[0-9]//g')"
 export CXXFLAGS="$(echo $CXXFLAGS | sed 's/-O[0-9]//g')"
 
