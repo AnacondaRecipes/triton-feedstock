de17cb505408b11c08c15c309bd0110a2f3dce64
Daniel Petry <dpetry@anaconda.com>
2024-04-02 15:40:37 -0500

    Add LLVM dir env variables

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,9 @@ export TRITON_LIBDEVICE_PATH=$PREFIX/lib/libdevice.10.bc
 export TRITON_PTXAS_PATH=$PREFIX/bin/ptxas
 export TRITON_CUOBJDUMP_PATH=$PREFIX/bin/cuobjdump
 export TRITON_NVDISASM_PATH=$PREFIX/bin/nvdisasm
+export LLVM_INCLUDE_DIRS=$PREFIX/include
+export LLVM_LIBRARY_DIR=$PREFIX/lib
+export LLVM_SYSPATH=$PREFIX
 
 cd python
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation
