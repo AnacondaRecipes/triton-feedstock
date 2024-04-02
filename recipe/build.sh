cf4f8b222d26b03ac4f7267b6e8458804b58efae
Daniel Petry <dpetry@anaconda.com>
2024-04-02 15:26:06 -0500

    Range of changes, untested

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,10 @@ set -ex
 # remove outdated vendored headers
 rm -rf $SRC_DIR/python/triton/third_party
 
+export TRITON_LIBDEVICE_PATH=$PREFIX/lib/libdevice.10.bc
+export TRITON_PTXAS_PATH=$PREFIX/bin/ptxas
+export TRITON_CUOBJDUMP_PATH=$PREFIX/bin/cuobjdump
+export TRITON_NVDISASM_PATH=$PREFIX/bin/nvdisasm
+
 cd python
-$PYTHON -m pip install . -vv
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation
