b9144012a8690b285aca800596b3ff4764169c09
Daniel Petry <dpetry@anaconda.com>
2024-04-04 17:10:32 -0500

    Various changes:
    - use the vendored-in llvm
    - search for cuda tools in the prefix at runtime (env variables used in build.sh would need to be in an activation script)
    - remove cuda stuff from recipe - needs to be reinstated later though, it will fail at runtime

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,10 +5,7 @@ set -ex
 # remove outdated vendored headers
 rm -rf $SRC_DIR/python/triton/third_party
 
-export TRITON_LIBDEVICE_PATH=$PREFIX/lib/libdevice.10.bc
-export TRITON_PTXAS_PATH=$PREFIX/bin/ptxas
-export TRITON_CUOBJDUMP_PATH=$PREFIX/bin/cuobjdump
-export TRITON_NVDISASM_PATH=$PREFIX/bin/nvdisasm
+export TRITON_BUILD_WITH_CCACHE=true
 
 cd python
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation
