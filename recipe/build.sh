c613d428d6dbcd0c88e3b4f2fd731ef97497c84e
Daniel Petry <dpetry@anaconda.com>
2024-04-09 15:38:25 -0500

    Don't use ccache

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,5 @@ set -ex
 # remove outdated vendored headers
 rm -rf $SRC_DIR/python/triton/third_party
 
-export TRITON_BUILD_WITH_CCACHE=true
-
 cd python
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation
