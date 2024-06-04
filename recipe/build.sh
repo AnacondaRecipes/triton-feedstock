9fb992d9e007c75cadb7f5af7e4c15ae603f494e
Daniel Petry <dpetry@anaconda.com>
2024-06-04 17:43:30 -0500

    Various fixes to fix build issues: find libstdc++, find and use cuda.h properly, point to correct location of libdevice.10.bc

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,8 @@ set -ex
 # remove outdated vendored headers
 rm -rf $SRC_DIR/python/triton/third_party
 
+# To find e.g. the right libstdc++
+export LD_LIBRARY_PATH=${PREFIX}/lib:$LD_LIBRARY_PATH
+
 cd python
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation
