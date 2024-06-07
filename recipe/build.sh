6c855dbc78589e831cc7a9bf73d917fb3d333888
Daniel Petry <dpetry@anaconda.com>
2024-06-07 14:09:17 -0500

    Don't remove third party folder; don't use cuda compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # remove outdated vendored headers
-rm -rf $SRC_DIR/python/triton/third_party
+#rm -rf $SRC_DIR/python/triton/third_party
 
 # To find e.g. the right libstdc++
 export LD_LIBRARY_PATH=${PREFIX}/lib:$LD_LIBRARY_PATH
