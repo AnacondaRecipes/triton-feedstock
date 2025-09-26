91f0c8c7977478912c76cf444e03d0d4b296efdb
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2025-09-26 20:13:33 +0200

    version 3.3.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,10 @@
 set -ex
 
 # remove outdated vendored headers
-rm -rf $SRC_DIR/python/triton/third_party
+#rm -rf $SRC_DIR/python/triton/third_party
 
 export JSON_SYSPATH=$PREFIX
-export LLVM_SYSPATH=$PREFIX
+#export LLVM_SYSPATH=$PREFIX
 export PYBIND11_SYSPATH=$SP_DIR/pybind11
 
 # these don't seem to be actually used, but they prevent downloads
@@ -14,10 +14,23 @@ export TRITON_PTXAS_PATH=$PREFIX/bin/ptxas
 export TRITON_CUOBJDUMP_PATH=$PREFIX/bin/cuobjdump
 export TRITON_NVDISASM_PATH=$PREFIX/bin/nvdisasm
 export TRITON_CUDACRT_PATH=$PREFIX
-export TRITON_CUDART_PATH=$PREFIX
+export TRITON_CUDART_PATH=$PREFIX/targets/x86_64-linux/include
 export TRITON_CUPTI_PATH=$PREFIX
 
 export MAX_JOBS=$CPU_COUNT
+
+echo "TRITON_CUDART_PATH files:"
+ls $TRITON_CUDART_PATH
+
+echo "=================="
+echo "cuda.h contents:"
+echo "=================="
+cat $TRITON_CUDART_PATH/cuda.h > ~/anc/cuda_h_cat.txt
+echo "=================="
+echo "end of cuda.h:"
+echo "=================="
+
+#export MLIR_DIR=$PREFIX/lib/cmake/mlir
 
 # the build does not run C++ unittests, and they implicitly fetch gtest
 # no easy way of passing this, not really worth a whole patch
